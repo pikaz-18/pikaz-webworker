74b4f1808a65a99ceb6a076bbe94394648c55c71
zouzheng <qq2363994579@outlook.com>
2020-05-25 14:49:52 +0800

    补齐git地址

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
  * @Author: zouzheng
  * @Date: 2020-05-25 10:54:46
  * @LastEditors: zouzheng
- * @LastEditTime: 2020-05-25 14:01:22
+ * @LastEditTime: 2020-05-25 14:31:22
  * @Description: 这是XXX组件（页面）
  */
 import SWorker from 'simple-web-worker'
 
 /**
  * @name: webworker运行函数
- * @param {arr} 传入数组对象
+ * @param {Object[]} arr/传入数组对象
  * @param {Function} fuc/数组对象函数
  * @param {Array} params/数组对象函数参数
  * @return: 
