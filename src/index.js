5d0235c996eb2caf221928ef814636fad7b967ce
zouzheng <qq2363994579@outlook.com>
2020-06-18 15:30:00 +0800

    修改webworker

--- src/index.js
+++ src/index.js
@@ -2,41 +2,42 @@
  * @Author: zouzheng
  * @Date: 2020-05-25 10:54:46
  * @LastEditors: zouzheng
- * @LastEditTime: 2020-05-25 14:31:22
+ * @LastEditTime: 2020-06-18 15:29:19
  * @Description: 这是XXX组件（页面）
  */
 import SWorker from 'simple-web-worker'
 
-/**
- * @name: webworker运行函数
- * @param {Object[]} arr/传入数组对象
- * @param {Function} fuc/数组对象函数
- * @param {Array} params/数组对象函数参数
- * @return: 
- */
-let w = null
-
-const run = arr => {
-  const actions = []
-  const func = []
-  arr.forEach((item, index) => {
-    actions.push({ message: index.toString(), func: item.fuc })
-    func.push({ message: index.toString(), args: item.params })
-  })
-  w = SWorker.create(actions)
-  return w.postAll(func)
-    .then(res => {
-      return res
+class WebWorker {
+  constructor() {
+    this.w = null
+  }
+  /**
+  * @name: webworker运行函数
+  * @param {Object[]} arr/传入数组对象
+  * @param {Function} fuc/数组对象函数
+  * @param {Array} params/数组对象函数参数
+  * @return:
+  */
+  run (arr) {
+    const actions = []
+    const func = []
+    arr.forEach((item, index) => {
+      actions.push({ message: index.toString(), func: item.fuc })
+      func.push({ message: index.toString(), args: item.params })
     })
-    .catch(err => {
-      return err
-    }).finally(() => {
-      w = null
-    })
+    this.w = SWorker.create(actions)
+    return this.w.postAll(func)
+      .then(res => {
+        return res
+      })
+      .catch(err => {
+        return err
+      }).finally(() => {
+        this.w = null
+      })
+  }
 }
 
-// 留作扩展
-const worker = {
-  run
-}
+const worker = new WebWorker()
+
 export default worker
